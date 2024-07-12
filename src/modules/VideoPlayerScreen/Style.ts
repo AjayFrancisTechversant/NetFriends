2e13e71d49d61ec7079233806d920ed67b94bee1
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-07-12 14:55:09 +0530

    .

--- src/modules/VideoPlayerScreen/Style.ts
+++ src/modules/VideoPlayerScreen/Style.ts
@@ -7,7 +7,7 @@ const styles = (screenContext:ScreenContextType, width:number, height:number) =>
     canvas: {flex: 1, backgroundColor: ColorPalette.white},
     menuButton: {position: 'absolute', top: height * 0.01, left: height * 0.01},
     heading:{fontSize:20,fontWeight:'bold',alignSelf:'center',margin:height*0.01},
-    thumbnailButtonContainer:{height:width*0.6,width:width*0.6,borderWidth:1,alignSelf:'center',margin:height*0.1,borderRadius:10,justifyContent:'center',alignItems:'center',borderRadius:10,overflow:'hidden'},
+    thumbnailButtonContainer:{height:width*0.6,width:width*0.6,borderWidth:1,alignSelf:'center',margin:height*0.1,borderRadius:10,justifyContent:'center',alignItems:'center',overflow:'hidden'},
     backgroundImageStyle:{height:width*0.6,width:width*0.6,justifyContent:'center',alignItems:'center'}
   });
 export default styles;
