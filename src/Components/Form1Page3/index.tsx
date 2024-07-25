aaf8a6b22cceb959d522d5435c597b115b83e9bf
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-07-25 18:03:54 +0530

    page3 redux working..but no validation

--- src/Components/Form1Page3/index.tsx
+++ src/Components/Form1Page3/index.tsx
@@ -1,6 +1,5 @@
 import {View, Text, TouchableOpacity} from 'react-native';
 import React, {useCallback, useEffect, useState} from 'react';
-import uuid from 'react-native-uuid';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useScreenContext} from '../../Contexts/ScreenContext';
 import {SetStateType} from '../../Types/Types';
@@ -12,6 +11,7 @@ import {
   lockPagesFrom,
   removeEducationDetail,
   unlockPage,
+  updateEducationDetails,
 } from '../../Redux/Slices/Form1DataSlice';
 import styles from './style';
 
@@ -35,34 +35,14 @@ const Form1Page3: React.FC<Form1Page3PropsType> = ({
   const educationDetailsFromRedux = useAppSelector(
     state => state.Form1Data.educationDetails,
   );
-  const [educationDetails, setEducationDetails] = useState<
-    EducationDetailsType[]
-  >(educationDetailsFromRedux);
-  const [errors, setErrors] = useState<{
-    [id: string]: Partial<EducationDetailsType>;
-  }>({});
-
-  useEffect(() => {
-    setEducationDetails(educationDetailsFromRedux);
-  }, [educationDetailsFromRedux]);
 
   const handleEducationDetailsChange = useCallback(
     (index: number, name: keyof EducationDetailsType, value: string) => {
-      setEducationDetails(prevDetails => {
-        const newDetails: any = [...prevDetails];
-        newDetails[index][name] = value;
-        // Clear the specific field's error for this index
-        setErrors(prevErrors => ({
-          ...prevErrors,
-          [newDetails[index].id]: {
-            ...(prevErrors[newDetails[index].id] || {}),
-            [name]: undefined,
-          },
-        }));
-        return newDetails;
-      });
+      const updatedDetails = [...educationDetailsFromRedux];
+      updatedDetails[index] = { ...updatedDetails[index], [name]: value };
+      dispatch(updateEducationDetails(updatedDetails));      
     },
-    [],
+    [dispatch, educationDetailsFromRedux],
   );
 
   const handleAddEducationDetail = useCallback(() => {
@@ -72,50 +52,21 @@ const Form1Page3: React.FC<Form1Page3PropsType> = ({
   const handleRemoveEducationDetail = useCallback(
     (id: string) => {
       dispatch(removeEducationDetail(id));
-      // setErrors(prevErrors => {
-      //   const newErrors = {...prevErrors};
-      //   delete newErrors[id];
-      //   return newErrors;
-      // });
     },
     [dispatch],
   );
-
-  const validateForm = () => {
-    let isValid = true;
-    const newErrors: {[id: string]: Partial<EducationDetailsType>} = {};
-
-    educationDetails.forEach(detail => {
-      const detailErrors: Partial<EducationDetailsType> = {};
-      if (!detail.institution)
-        detailErrors.institution = 'Institution is required';
-      if (!detail.degree) detailErrors.degree = 'Degree is required';
-      if (!detail.fieldOfStudy)
-        detailErrors.fieldOfStudy = 'Field of study is required';
-      if (!detail.yearOfCompletion)
-        detailErrors.yearOfCompletion = 'Year of completion is required';
-
-      if (Object.keys(detailErrors).length) {
-        isValid = false;
-        newErrors[detail.id] = detailErrors;
-      }
-    });
-
-    setErrors(newErrors);
-    return isValid;
-  };
 
   const handleGoBack = () => {
     setSegmentedButtonValue('2');
   };
 
   const handleSave = () => {
-    if (!validateForm()) {
-      dispatch(unlockPage(4));
-      setSegmentedButtonValue('4');
-    } else {
-      dispatch(lockPagesFrom(4));
-    }
+    // if (!validateForm()) {
+    //   dispatch(unlockPage(4));
+    //   setSegmentedButtonValue('4');
+    // } else {
+    //   dispatch(lockPagesFrom(4));
+    // }
   };
 
   const screenContext = useScreenContext();
@@ -135,8 +86,7 @@ const Form1Page3: React.FC<Form1Page3PropsType> = ({
   return (
     <View>
       <Text style={screenStyles.subHeading}>Educational Details:</Text>
-      {educationDetails.length &&
-        educationDetails.map((education, index) => (
+      {educationDetailsFromRedux.map((education, index) => (
           <View key={education.id} style={screenStyles.educationDetailsCard}>
             {education.isExtra && (
               <View style={screenStyles.AddiEduHeaderContainer}>
@@ -158,12 +108,7 @@ const Form1Page3: React.FC<Form1Page3PropsType> = ({
                 handleEducationDetailsChange(index, 'institution', text)
               }
             />
-            {errors[education.id]?.institution && (
-              <Text style={screenStyles.errorText}>
-                {errors[education.id]?.institution}
-              </Text>
-            )}
-
+          
             {renderLabel('Degree', true)}
             <MyTextInput
               style={screenStyles.textInput}
@@ -172,12 +117,6 @@ const Form1Page3: React.FC<Form1Page3PropsType> = ({
                 handleEducationDetailsChange(index, 'degree', text)
               }
             />
-            {errors[education.id]?.degree && (
-              <Text style={screenStyles.errorText}>
-                {errors[education.id]?.degree}
-              </Text>
-            )}
-
             {renderLabel('Field of Study', true)}
             <MyTextInput
               style={screenStyles.textInput}
@@ -186,12 +125,7 @@ const Form1Page3: React.FC<Form1Page3PropsType> = ({
                 handleEducationDetailsChange(index, 'fieldOfStudy', text)
               }
             />
-            {errors[education.id]?.fieldOfStudy && (
-              <Text style={screenStyles.errorText}>
-                {errors[education.id]?.fieldOfStudy}
-              </Text>
-            )}
-
+        
             {renderLabel('Year of Completion', true)}
             <MyTextInput
               style={screenStyles.textInput}
@@ -200,11 +134,7 @@ const Form1Page3: React.FC<Form1Page3PropsType> = ({
                 handleEducationDetailsChange(index, 'yearOfCompletion', text)
               }
             />
-            {errors[education.id]?.yearOfCompletion && (
-              <Text style={screenStyles.errorText}>
-                {errors[education.id]?.yearOfCompletion}
-              </Text>
-            )}
+         
           </View>
         ))}
       <TouchableOpacity
